629e74a162acc2fd4eb2bcc881ca0ef206f9e580
League2EB <screamlazy@gmail.com>
2019-10-25 02:55:17 +0800

    [Fix] 修正執行序括號寫錯問題....

--- HelloMyRequest/ViewModel.swift
+++ HelloMyRequest/ViewModel.swift
@@ -21,33 +21,33 @@ class ViewModel {
     func featchData(completionHandler: @escaping (CompletionHandler)) {
         queue.async {
             print("執行序在：\(self.queue.label)")
-        }
-        //宣告網路請求物件
-        let session = URLSession(configuration: .default)
-        //請求HOST
-        let url = "https://raw.githubusercontent.com/wakeuparts/api/master/location.json"
-        //宣告網路請求對象，就是塞你要送的資料
-        let UrlRequest = URLRequest(url: URL(string: url)!)
-        //請求
-        let task = session.dataTask(with: UrlRequest) { (data, response, error) in
-            DispatchQueue.main.sync {
-                do {
-                    //解析字典
-                    _ = try JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
-                    //查看當前執行序
-                    if let currentDispatch = OperationQueue.current?.underlyingQueue {
-                        print("執行序在：\(currentDispatch)")
+            //宣告網路請求物件
+            let session = URLSession(configuration: .default)
+            //請求HOST
+            let url = "https://raw.githubusercontent.com/wakeuparts/api/master/location.json"
+            //宣告網路請求對象，就是塞你要送的資料
+            let UrlRequest = URLRequest(url: URL(string: url)!)
+            //請求
+            let task = session.dataTask(with: UrlRequest) { (data, response, error) in
+                DispatchQueue.main.sync {
+                    do {
+                        //解析字典
+                        _ = try JSONSerialization.jsonObject(with: data!, options: []) as! NSDictionary
+                        //查看當前執行序
+                        if let currentDispatch = OperationQueue.current?.underlyingQueue {
+                            print("執行序在：\(currentDispatch)")
+                        }
+                        completionHandler("成功？")
+                    } catch {
+                        completionHandler("失敗？")
+                        print("Error")
+                        return
                     }
-                    completionHandler("成功？")
-                } catch {
-                    completionHandler("失敗？")
-                    print("Error")
-                    return
                 }
             }
+            //開始任務
+            task.resume()
         }
-        //開始任務
-        task.resume()
     }
 }
 
